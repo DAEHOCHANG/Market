7af48909d8d18fe415be1c00094124d376af5ebb
DaeHoChang <eogh1209@gmail.com>
2021-11-06 23:16:14 +0900

    [기능] 품목 추가시 잘못된 경우 경고창

--- Market/ViewController/ProductAppendingViewController.swift
+++ Market/ViewController/ProductAppendingViewController.swift
@@ -31,19 +31,29 @@ class ProductAppendingViewController: UIViewController {
     }
     
     @IBAction func plusButton(_ sender: UIButton) {
-        defer { self.presentingViewController?.dismiss(animated: true) }
-        
-        guard let day = appendingDay, let viewModel = calendarViewModel else { return }
-        guard let name = name.text, let quantStr = number.text else { return }
+        guard let day = appendingDay, let viewModel = calendarViewModel else { plusFailFunction(1);return }
+        guard let name = name.text, let quantStr = number.text else { plusFailFunction(2);return }
         
         let quantityS = quantStr.components(separatedBy: CharacterSet.letters).joined().trimmingCharacters(in: .whitespaces)
         let unit = quantStr.components(separatedBy: CharacterSet.decimalDigits).joined().trimmingCharacters(in: .whitespaces)
         
-        guard let quantity = Int(quantityS) else { return }
+        guard let quantity = Int(quantityS) else { plusFailFunction(3);return }
         
         let product = MarketProduct(product: tmpProduct(productName: name,unit: unit), productQuantity: quantity)
         viewModel.appendProduct(when: day, product: product)
         historyViewModel?.appendProduct(product: product.product)
+        self.presentingViewController?.dismiss(animated: true)
     }
-    
+    func plusFailFunction(_ num: Int) {
+        var message = ""
+        if num == 3 {
+            message = "수량 단위 형식으로 입력해 주세요\nex) 30kg, 30 kg, 1근, 30개"
+        } else {
+            message = "예상하지 못한 에러발생 종료후 다시 시도해보세요"
+        }
+        let failAlert = UIAlertController(title: "입력 실패", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        failAlert.addAction(okAction)
+        self.present(failAlert, animated: true, completion: nil)
+    }
 }
